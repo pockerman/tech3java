689436c47f96767e03bfb89841339693e5e18017
David Hilbert <a.giavaras@gmail.com>
2019-08-15 10:06:51 +0300

    First commit for tech3java

---
+++ src/main/java/applications/algorithms/FindFirstOccurenceString.java
@@ -0,0 +1,56 @@
+package applications.algorithms;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Find the first occurence of a non-repeated
+ * character in a string
+ */
+public class FindFirstOccurenceString {
+
+
+    public static void findChar(String input) {
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        // populate the hash map
+        for (int i = 0; i < input.length(); ++i) {
+
+            char character = input.charAt(i);
+
+            if (!map.containsKey(character)) {
+
+                map.put(character, 1);
+            } else {
+
+                map.put(character, map.get(character) + 1);
+            }
+        }
+
+        // one more loop to find the first non-repeated character
+        for (int i = 0; i < input.length(); ++i) {
+
+            char character = input.charAt(i);
+
+            if (map.get(character) == 1) {
+                System.out.println("First non repeated character in " + input + " is: " + character);
+                break;
+            }
+
+        }
+    }
+
+    public static void main(String[] args){
+
+        // read the input string from user
+        Scanner scanner = new Scanner(System. in);
+        String inputString = scanner.nextLine();
+
+        System.out.println("You entered: " + inputString);
+
+        findChar(inputString);
+
+
+    }
+}
