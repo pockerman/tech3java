6246e2cc01d64914450ff54ee84d4e363964d051
David Hilbert <a.giavaras@gmail.com>
2019-08-16 07:58:22 +0300

    Add FindFirstNonRepeatedCharInString application

---
+++ src/main/java/applications/algorithms/FindFirstNonRepeatedCharInString.java
@@ -0,0 +1,61 @@
+package applications.algorithms;
+import java.util.Scanner;
+
+/**
+ * Finds the first non-repeated character in a
+ * user specified string. It assumes ASCII characters
+ */
+
+public class FindFirstNonRepeatedCharInString {
+
+    public static void find(String str){
+
+        if(str == null){
+
+            throw new NullPointerException("Input string instance in null");
+        }
+
+        if(str == ""){
+
+            System.out.println("Empty string was specified....");
+            return;
+        }
+
+        // we can have as much as 256 ASCII characters
+        int[] vals = new int[256];
+
+        for(int i=0; i<vals.length; ++i){
+            vals[i] = 0;
+        }
+
+        for(int i=0; i<str.length(); ++i){
+
+            int val = str.charAt(i);
+            vals[val] += 1;
+        }
+
+        for(int i=0; i<str.length(); ++i){
+
+            int val = str.charAt(i);
+            if(vals[val] == 1){
+
+                System.out.println("First non repeated character is: "+str.charAt(i));
+                break;
+            }
+        }
+
+    }
+
+
+    public static void main(String[] arg){
+
+        Scanner scanner = new Scanner(System. in);
+        String inputString = scanner.nextLine();
+
+        System.out.println("You entered: " + inputString);
+
+        find(inputString);
+
+
+    }
+}
