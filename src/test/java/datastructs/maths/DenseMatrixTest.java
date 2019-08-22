3f066232ad64481ff0ed9f894b1d23ff0d3fbf43
Alexandros Giavaras <a.giavaras@gmail.com>
2019-08-22 05:58:27 +0100

    Add DenseMatrixTest

---
+++ src/test/java/datastructs/maths/DenseMatrixTest.java
@@ -0,0 +1,7 @@
+package datastructs.maths;
+
+/**
+ * Unit tests for DenseMatrix class
+ */
+public class DenseMatrixTest {
+}
---
+++ src/test/java/datastructs/maths/DenseMatrixTestRunner.java
@@ -0,0 +1,13 @@
+package datastructs.maths;
+
+public class DenseMatrixTestRunner {
+
+    public static void run(String[] args){
+
+    }
+
+    public static void main(String[] args){
+        DenseMatrixTestRunner.run(args);
+
+    }
+}
