f452871cd949e6c7bff25e063432e25cc79be46e
David Hilbert <a.giavaras@gmail.com>
2019-10-09 11:28:42 +0300

    #14 fix bugs

--- src/main/java/rl/IterativePolicyEvaluation.java
+++ src/main/java/rl/IterativePolicyEvaluation.java
@@ -20,11 +20,39 @@ public class IterativePolicyEvaluation {
     /**
      * Run the evaluation
      */
-    public void evaluate(IStateSpace<IState> space){
+    public <T extends IState> void evaluate(IStateSpace<T> space){
 
+        //initialize V and V prime
+        this.initialize(space.nStates());
 
     }
 
+    /**
+     * Zero V and V prime
+     */
+    public final void zero(){
+
+        for (int i = 0; i < this.v.length; i++) {
+
+            this.v[i] = 0.0;
+            this.vPrime[i] = 0.0;
+
+        }
+    }
+
+    /**
+     * Initialize V and V prime to zero
+     */
+    protected final void initialize(int length){
+
+        if( this.v.length != length || this.vPrime.length != length){
+
+            this.v = new double[length];
+            this.vPrime = new double[length];
+        }
+
+        this.zero();
+    }
 
     /**
      * Parameters used by the algorithm
