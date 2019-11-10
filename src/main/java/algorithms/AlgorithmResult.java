a37f2e77100db131d2139207e4eb012753437b42
David Hilbert <a.giavaras@gmail.com>
2019-11-10 17:12:46 +0200

    #90 Add classes to facilitate algorithm I/O

---
+++ src/main/java/algorithms/AlgorithmResult.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+import java.util.Timer;
+
+public class AlgorithmResult {
+
+    public boolean converged=false;
+    public double tolerance;
+    public int numThreadsUsed;
+    public int numIterationsNeeded;
+
+}
