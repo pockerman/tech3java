851322caf24eeaded314892a18a7485e7a542d86
David Hilbert <a.giavaras@gmail.com>
2019-11-22 15:48:28 +0200

    #144 Add range partitioner

---
+++ src/main/java/parallel/partitioners/RangePartitioner.java
@@ -0,0 +1,43 @@
+package parallel.partitioners;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RangePartitioner {
+
+
+    public static  List<List<Integer>> rangePartitioner(int start, int end, int nParts){
+
+        List<List<Integer>> partitions = new ArrayList<>();
+
+        if(nParts ==0 ){
+
+            partitions.add(new ArrayList<>());
+
+            for (int i = start; i < end; i++) {
+                partitions.get(0).add(i);
+            }
+
+            return partitions;
+        }
+
+        int workSize = (end-start)/nParts;
+        int partStart = 0;
+        int partEnd = workSize;
+        for (int i = 0; i < nParts; i++) {
+
+            partitions.add(new ArrayList<>());
+
+            for (int p = partStart; p < partEnd; ++p) {
+                partitions.get(i).add(p);
+            }
+
+            partStart = partEnd;
+            partEnd += workSize;
+        }
+
+        // collect what is left
+
+        return partitions;
+    }
+}
